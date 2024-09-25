8060e716b111f549233fc973cbf849327244fe46
iam-mayank-kansal <mkstarmayank221@gmail.com>
2024-09-25 12:44:13 +0530

    does this for live

--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { ShowHomePage, GetURL, GiveShortUrl , RedirectToUrl} = require("../controllers/controller");
+const { ShowHomePage, GetURL, GiveShortUrl, RedirectToUrl } = require("../controllers/controller");
 
 router.get("/", ShowHomePage);
+router.get("https://url-shortner-nine-teal.vercel.app/", ShowHomePage);
+
 router.get("/urlPage", GetURL);
+router.get("https://url-shortner-nine-teal.vercel.app/urlPage", GetURL);
+
 router.post("/urlPage", GiveShortUrl);
+router.post("https://url-shortner-nine-teal.vercel.app/urlPage", GiveShortUrl);
+
 router.get("/:shortID", RedirectToUrl);
+router.get("https://url-shortner-nine-teal.vercel.app/:shortID", RedirectToUrl);
 
 module.exports = { router };
