609e3e766c25cb435cf578357be760bf0a719ea8
iam-mayank-kansal <mkstarmayank221@gmail.com>
2024-09-25 12:53:15 +0530

    vercel setting

--- routes/route.js
+++ routes/route.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const { ShowHomePage, GetURL, GiveShortUrl, RedirectToUrl } = require("../controllers/controller");
 
 router.get("/", ShowHomePage);
-router.get("https://url-shortner-nine-teal.vercel.app/", ShowHomePage);
+router.get("https://url-shortner-oxkn1dibo-mayanks-projects-b06bc3e0.vercel.app/", ShowHomePage);
 
 router.get("/urlPage", GetURL);
-router.get("https://url-shortner-nine-teal.vercel.app/urlPage", GetURL);
+router.get("https://url-shortner-oxkn1dibo-mayanks-projects-b06bc3e0.vercel.app/urlPage", GetURL);
 
 router.post("/urlPage", GiveShortUrl);
-router.post("https://url-shortner-nine-teal.vercel.app/urlPage", GiveShortUrl);
+router.post("https://url-shortner-oxkn1dibo-mayanks-projects-b06bc3e0.vercel.app/urlPage", GiveShortUrl);
 
 router.get("/:shortID", RedirectToUrl);
-router.get("https://url-shortner-nine-teal.vercel.app/:shortID", RedirectToUrl);
+router.get("https://url-shortner-oxkn1dibo-mayanks-projects-b06bc3e0.vercel.app/:shortID", RedirectToUrl);
 
 module.exports = { router };
